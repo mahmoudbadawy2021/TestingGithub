d4cef3399bdf1f4f51f36eea707e26d7b06455e0
mahmoudbadawy5 <mahmoudbadawy2011@gmail.com>
2021-03-06 14:06:32 +0200

    Added my name.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("mazen yousry abd el azim 18101326");
         System.out.println("Khaled Amin 18101823");
+        System.out.println("Mahmoud Mohamed Ibrahim Badawy 18103114");
     }
 }
